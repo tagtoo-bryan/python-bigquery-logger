63f5e871414a8f3bdc11c22bfa83b06e5286fa2b
bryan <bryan@tagtoo.org>
2015-03-05 13:45:54 +0800

    init, copy pyslack to be template

---
+++ python-bigquery-logger/__init__.py
@@ -0,0 +1,60 @@
+import logging
+import requests
+import json
+
+class SlackError(Exception):
+    pass
+
+
+class SlackClient(object):
+
+    BASE_URL = 'https://hooks.slack.com/services/'
+
+    def __init__(self, token):
+        self.token = token
+
+    def _make_request(self, method, params):
+        """Make request to API endpoint
+
+        Note: Ignoring SSL cert validation due to intermittent failures
+        http://requests.readthedocs.org/en/latest/user/advanced/#ssl-cert-verification
+        """
+        url = "%s/%s" % (SlackClient.BASE_URL, self.token)
+        result = requests.post(url, data={"payload": json.dumps(params)}, verify=False)
+        # if not result['ok']:
+        #     raise SlackError(result['error'])
+        return result
+
+    def chat_post_message(self, channel, text, **params):
+        """chat.postMessage
+
+        This method posts a message to a channel.
+
+        Check docs for all available **params options:
+        https://api.slack.com/methods/chat.postMessage
+        """
+        method = 'chat.postMessage'
+        params.update({
+            'channel': channel,
+            'text': text,
+        })
+        return self._make_request(method, params)
+
+
+class SlackHandler(logging.Handler):
+    """A logging handler that posts messages to a Slack channel!
+
+    References:
+    http://docs.python.org/2/library/logging.html#handler-objects
+    """
+    def __init__(self, token, channel, **kwargs):
+        super(SlackHandler, self).__init__()
+        self.client = SlackClient(token)
+        self.channel = channel
+        self._kwargs = kwargs
+
+    def emit(self, record):
+        message = self.format(record)
+        self.client.chat_post_message(self.channel,
+                                      message,
+                                      **self._kwargs)
